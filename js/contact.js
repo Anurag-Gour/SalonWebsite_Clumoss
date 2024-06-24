59a2d7197109a7d5bfd18e2f8cf4fb83608aecff
Manas <manasarora371@gmail.com>
2024-06-25 00:09:06 +0530

    Contact page changes

--- js/contact.js
+++ js/contact.js
@@ -2,3 +2,13 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Your message has been sent!');
 });
+
+function validatePhoneInput(event) {
+    const input = event.target;
+    input.value = input.value.replace(/[^0-9]/g, '');
+}
+
+window.onload = function() {
+    const phoneInput = document.getElementById('phone');
+    phoneInput.addEventListener('input', validatePhoneInput);
+};
